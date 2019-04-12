c6b22bdae66c6b9f5d256ff30bc9b2a4d5c72fa3
hetpatel07 <hets7878401023@gmail.com>
2019-04-12 20:16:18 +0530

    star

---
+++ js/main.js
@@ -0,0 +1,94 @@
+var a = [];
+
+for (i = 0; i < 11; i++) {
+    for (j = 0; j < 11; j++) {
+        document.write('&nbsp&nbsp \t');
+        if (i == 0 && j == 4) {
+            document.write('&nbsp&nbsp \t');
+        }
+       else if (i == 0 && j == 5) {
+            document.write('h \t');
+        } else if (i == 1 && j == 4) {
+            document.write('h \t');
+        } else if (i == 1 && j == 5) {
+            document.write('h \t');
+        } else if (i == 1 && j == 6) {
+            document.write('h \t');
+        } else if (i == 2 && j == 3) {
+            document.write('h \t');
+        } else if (i == 2 && j == 5) {
+            document.write('h \t');
+        } else if (i == 2 && j == 7) {
+            document.write('h \t');
+        } else if (i == 3 && j == 2) {
+            document.write('h \t');
+        } else if (i == 3 && j == 4) {
+            document.write('&nbsp \t');
+        } else if (i == 3 && j == 6) {
+            document.write('&nbsp \t');
+        } else if (i == 3 && j == 8) {
+            document.write('h \t');
+        } else if (i == 4 && j == 1) {
+            document.write('h \t');
+        } else if (i == 4 && j == 3) {
+            document.write('&nbsp \t');
+        } else if (i == 4 && j == 5) {
+            document.write('&nbsp \t');
+        } else if (i == 4 && j == 7) {
+            document.write('&nbsp \t');
+        } else if (i == 4 && j == 9) {
+            document.write('h \t');
+        } else if (i == 5 && j == 0) {
+            document.write('h \t');
+        } else if (i == 5 && j == 2) {
+            document.write('&nbsp \t');
+        } else if (i == 5 && j == 4) {
+            document.write('&nbsp \t');
+        } else if (i == 5 && j == 6) {
+            document.write('&nbsp \t');
+        } else if (i == 5 && j == 8) {
+            document.write('&nbsp \t');
+        } else if (i == 5 && j == 10) {
+            document.write('h \t');
+        } else if (i == 6 && j == 1) {
+            document.write('h \t');
+        } else if (i == 6 && j == 3) {
+            document.write('&nbsp \t');
+        } else if (i == 6 && j == 5) {
+            document.write('&nbsp \t');
+        } else if (i == 6 && j == 7) {
+            document.write('&nbsp \t');
+        } else if (i == 6 && j == 9) {
+            document.write('h \t');
+        } else if (i == 7 && j == 2) {
+            document.write('h \t');
+        } else if (i == 7 && j == 4) {
+            document.write('&nbsp \t');
+        } else if (i == 7 && j == 6) {
+            document.write('&nbsp \t');
+        } else if (i == 7 && j == 8) {
+            document.write('h \t');
+        }
+         else if (i == 8 && j == 3) {
+            document.write('h \t');
+        } else if (i == 8 && j == 5) {
+            document.write('h \t');
+        } else if (i == 8 && j == 7) {
+            document.write('h \t');
+        }
+         else if (i == 9 && j == 4) {
+            document.write('h \t');
+        } else if (i == 9 && j == 5) {
+            document.write('h \t');
+        } else if (i == 9 && j == 6) {
+            document.write('h \t');
+        }
+       else if (i == 10 && j == 5) {
+            document.write('h \t');
+        }
+        else if (i == 10 && j == 4) {
+            document.write('&nbsp&nbsp \t');
+        }
+    }
+    document.write('<br>');
+}
